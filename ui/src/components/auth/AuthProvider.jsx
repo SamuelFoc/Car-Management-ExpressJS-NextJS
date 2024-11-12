c5be0da45a64e0058d81b7c5edb47ee1850c6eb1
SamuelFoc <samo.sipikal@gmail.com>
2024-11-12 14:29:11 +0100

    Create authprovider component for JWT validity check

---
+++ ui/src/components/auth/AuthProvider.jsx
@@ -0,0 +1,44 @@
+"use client";
+import { apiFetch } from "@/utils/apiFetch";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function AuthProvider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/"); // Redirect to login if no token
+    } else {
+      validateToken(token);
+    }
+  }, [router]);
+
+  const validateToken = async (token) => {
+    try {
+      const response = await apiFetch("/auth/validate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.decoded) {
+        throw new Error("Token is not valid!");
+      }
+      setIsAuthenticated(true); // Token is valid
+    } catch (err) {
+      localStorage.removeItem("token");
+      router.push("/"); // Redirect to login on invalid token
+    }
+  };
+
+  // Render children only if authenticated
+  if (!isAuthenticated) {
+    return <div>Loading...</div>;
+  }
+
+  return <>{children}</>;
+}
