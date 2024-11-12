b4fd5e1132476997d508a148580eed6282744847
SamuelFoc <samo.sipikal@gmail.com>
2024-11-12 14:36:00 +0100

    Create Heading component

---
+++ ui/src/components/layout/heading/Heading.jsx
@@ -0,0 +1,7 @@
+export default function Heading() {
+  return (
+    <h1 className="w-full items-start text-3xl font-bold">
+      <span className="text-secondary font-black">V</span> - Manager
+    </h1>
+  );
+}
