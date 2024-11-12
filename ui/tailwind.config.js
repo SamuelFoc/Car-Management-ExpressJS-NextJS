15ac2f18dc4f5d499c6dd6f4b14b788d59d85ed7
SamuelFoc <samo.sipikal@gmail.com>
2024-11-12 14:26:53 +0100

    Add light version of secondary color

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#1f2534",
+        secondary: {
+          light: "#ff8252",
+          DEFAULT: "#e56c00",
+          dark: "#e56c00",
+        },
+        accent: {
+          light: "#F7B84B", // Light orange
+          DEFAULT: "#FF8C00", // Vibrant orange
+          dark: "#C86400", // Deeper orange
+        },
       },
     },
   },
