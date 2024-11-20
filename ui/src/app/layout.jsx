098bc7c9835d7489bdd69535a3c1eb5cd5819dc8
SamuelFoc <samo.sipikal@gmail.com>
2024-11-20 08:36:56 +0100

    Add base layout

--- ui/src/app/layout.jsx
+++ ui/src/app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary text-white w-screen h-screen p-3 py-4 flex items-start justify-center">
+      <body className="bg-primary text-white w-full h-screen p-3 py-4 flex items-start justify-center">
         {children}
       </body>
     </html>
