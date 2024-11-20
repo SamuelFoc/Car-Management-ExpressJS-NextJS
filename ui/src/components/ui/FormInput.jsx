c782582e4d34680e9e64583b7b0ada4a80955959
SamuelFoc <samo.sipikal@gmail.com>
2024-11-20 11:35:05 +0100

    Add * sign to highlight required Input

--- ui/src/components/ui/FormInput.jsx
+++ ui/src/components/ui/FormInput.jsx
@@ -1,5 +1,6 @@
 export default function FormInput({
   children,
+  id,
   type,
   value,
   placeholder,
@@ -7,13 +8,15 @@ export default function FormInput({
   req,
 }) {
   return (
-    <div className="mb-4">
+    <div className="mb-4 relative">
       <label className="block text-gray-600 text-sm mb-2">{children}</label>
+      {req && <span className="absolute -top-0 -left-2 text-red-600">*</span>}
       <input
+        id={id || ""}
         type={type}
         value={value}
         onChange={onChange}
-        className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-primary"
+        className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-primary text-primary"
         placeholder={placeholder}
         required={req}
       />
