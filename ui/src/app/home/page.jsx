e8515aeb7cbd50d80d67ca4c0272b307f9a86b94
SamuelFoc <samo.sipikal@gmail.com>
2024-11-20 11:32:47 +0100

    Add Navbar and switch to MyCars C from Cars C

--- ui/src/app/home/page.jsx
+++ ui/src/app/home/page.jsx
@@ -1,17 +1,15 @@
-import AddCar from "@/components/function/Cars/addCar";
-import Cars from "@/components/function/Cars/Cars";
-import UserDetails from "@/components/function/User/UserDetails";
+import MyCars from "@/components/functional/Cars/MyCars";
+import UserDetails from "@/components/functional/User/UserDetails";
 import SecureContainer from "@/components/layout/container/SecureContainer";
 import Footer from "@/components/layout/footer/Footer";
-import Heading from "@/components/layout/heading/Heading";
+import NavBar from "@/components/ui/Navbar";
 
 export default function Home() {
   return (
     <SecureContainer>
-      <Heading href={"/home"} />
+      <NavBar href={"/home"} />
       <UserDetails />
-      <AddCar />
-      <Cars />
+      <MyCars />
       <Footer />
     </SecureContainer>
   );
