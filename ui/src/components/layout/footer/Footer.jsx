71ca86820f2f70ae55d6c98d2338fc0a1d7aedb5
SamuelFoc <samo.sipikal@gmail.com>
2024-11-20 08:36:03 +0100

    Add Footer C

---
+++ ui/src/components/layout/footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="h-40 w-full flex items-center justify-center">
+      <p className="text-gray-600">
+        © 2024 Ing. Samuel Šipikal. All rights reserved.
+      </p>
+    </footer>
+  );
+}
