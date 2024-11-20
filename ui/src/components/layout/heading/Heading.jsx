c495dd62dd86ba80492ae75599976bc122f07e23
SamuelFoc <samo.sipikal@gmail.com>
2024-11-20 08:39:29 +0100

    Add Logout button to Heading C

--- ui/src/components/layout/heading/Heading.jsx
+++ ui/src/components/layout/heading/Heading.jsx
@@ -1,7 +1,23 @@
-export default function Heading() {
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Heading({ href }) {
+  const router = useRouter();
+  const logout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
   return (
-    <h1 className="w-full items-start text-3xl font-bold">
-      <span className="text-secondary font-black">V</span> - Manager
-    </h1>
+    <Link
+      href={href || "/"}
+      className="w-full items-start text-3xl font-bold mb-5 flex justify-between"
+    >
+      <h1>
+        <span className="text-secondary font-black">V</span> - Manager
+      </h1>
+      <div onClick={logout}>🚪</div>
+    </Link>
   );
 }
