e4754f9233cc7b7a448f828a3d88fa61fbbea312
SamuelFoc <samo.sipikal@gmail.com>
2024-11-20 08:37:49 +0100

    Create Home page

--- ui/src/app/home/page.jsx
+++ ui/src/app/home/page.jsx
@@ -1,14 +1,18 @@
+import AddCar from "@/components/function/Cars/addCar";
 import Cars from "@/components/function/Cars/Cars";
 import UserDetails from "@/components/function/User/UserDetails";
 import SecureContainer from "@/components/layout/container/SecureContainer";
+import Footer from "@/components/layout/footer/Footer";
 import Heading from "@/components/layout/heading/Heading";
 
 export default function Home() {
   return (
     <SecureContainer>
-      <Heading />
+      <Heading href={"/home"} />
       <UserDetails />
-      <Cars></Cars>
+      <AddCar />
+      <Cars />
+      <Footer />
     </SecureContainer>
   );
 }
