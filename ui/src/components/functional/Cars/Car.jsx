34b308f2763ee0d90bb3a2c92d7c10681366ba17
SamuelFoc <samo.sipikal@gmail.com>
2024-11-20 11:33:47 +0100

    Change the width of the Car C card from 64 to 56

---
+++ ui/src/components/functional/Cars/Car.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Car({ car }) {
+  return (
+    <Link
+      href={`/cars/${car?.id}`}
+      className="bg-gray-100 hover:bg-gray-600 hover:bg-opacity-5 bg-opacity-5 rounded-md w-56 p-4 flex flex-col items-center justify-center gap-1 hover:cursor-pointer relative"
+    >
+      <Image src={"/car_1.png"} width={150} height={150} alt="car" />
+      <h1 className="text-gray-200">
+        <strong>{car?.make}</strong>&ensp;
+        <strong>{car?.model}</strong>
+        &ensp;<span className="opacity-50">({car?.year})</span>
+      </h1>
+      <div className="text-gray-500 w-fit flex items-center justify-center px-3 py-1 rounded-sm font-black absolute top-0 left-0">
+        {car?.licensePlate}
+      </div>
+    </Link>
+  );
+}
