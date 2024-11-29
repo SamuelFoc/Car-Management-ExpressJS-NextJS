a7d50184d7aa2798bad774cdb1557c2b6dc60892
SamuelFoc <samo.sipikal@gmail.com>
2024-11-29 14:35:47 +0100

    Add navbar and footer to the SecureContainer component

--- ui/src/app/home/page.jsx
+++ ui/src/app/home/page.jsx
@@ -1,16 +1,12 @@
 import MyCars from "@/components/functional/Cars/MyCars";
 import UserDetails from "@/components/functional/User/UserDetails";
 import SecureContainer from "@/components/layout/container/SecureContainer";
-import Footer from "@/components/layout/footer/Footer";
-import NavBar from "@/components/ui/Navbar";
 
 export default function Home() {
   return (
     <SecureContainer>
-      <NavBar href={"/home"} />
       <UserDetails />
       <MyCars />
-      <Footer />
     </SecureContainer>
   );
 }
--- ui/src/components/layout/container/SecureContainer.jsx
+++ ui/src/components/layout/container/SecureContainer.jsx
@@ -1,9 +1,15 @@
 import AuthProvider from "@/components/auth/AuthProvider";
+import Footer from "../footer/Footer";
+import NavBar from "@/components/ui/Navbar";
 
 export default function SecureContainer({ children }) {
   return (
     <div className="w-full max-w-3xl flex flex-col gap-5">
-      <AuthProvider>{children}</AuthProvider>
+      <AuthProvider>
+        <NavBar href={"/home"} />
+        {children}
+        <Footer />
+      </AuthProvider>
     </div>
   );
 }
