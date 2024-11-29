87a328e8855b9c6a6e76f1fa357aa4cc6e54ec73
SamuelFoc <samo.sipikal@gmail.com>
2024-11-29 14:35:20 +0100

    Modularize car specific page code into components

--- ui/src/app/cars/[carId]/page.jsx
+++ ui/src/app/cars/[carId]/page.jsx
@@ -1,108 +1,19 @@
 "use client";
-import CarDetailBlock from "@/components/functional/Cars/CarDetailBlock";
-import Services from "@/components/functional/Services/Services";
-import SecureContainer from "@/components/layout/container/SecureContainer";
-import Footer from "@/components/layout/footer/Footer";
-import BackButton from "@/components/ui/BackButton";
-import NavBar from "@/components/ui/Navbar";
-import Row from "@/components/ui/Row";
-import Title from "@/components/ui/Title";
-import { apiFetch } from "@/utils/apiFetch";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import CarDetails from "@/components/functional/Cars/CarDetails";
+import CarDocuments from "@/components/functional/Documents/CarDocuments";
+import SecureContainer from "@/components/layout/container/SecureContainer";
+import Services from "@/components/functional/Services/Services";
 
-export default function CarDetails() {
+export default function CarDetail() {
   const { carId } = useParams();
-  const [carDetails, setCarDetails] = useState();
-
-  useEffect(() => {
-    const fetchCarDetails = async () => {
-      try {
-        const res = await apiFetch(`/cars/${carId}`);
-        setCarDetails(res?.car);
-      } catch (error) {
-        console.error("Error fetching user details:", error.message);
-      }
-    };
-
-    fetchCarDetails();
-  }, []);
 
   return (
     <SecureContainer>
-      <NavBar href={"/home"} />
-      <Row>
-        <Title subtitle={carDetails?.year}>
-          {carDetails?.make + " " + carDetails?.model}
-        </Title>
-        <BackButton>◀</BackButton>
-      </Row>
-      <CarDetailBlock
-        image={"/info-icon.png"}
-        carId={carId}
-        details={[
-          {
-            label: "License Plate",
-            key: "licensePlate",
-            data: carDetails?.licensePlate,
-          },
-          {
-            label: "VIN",
-            key: "vin",
-            data: carDetails?.vin,
-          },
-          {
-            label: "Mileage [km]",
-            key: "mileage",
-            data: carDetails?.mileage,
-          },
-        ]}
-      />
-      <CarDetailBlock
-        image={"/engine-icon.png"}
-        carId={carId}
-        details={[
-          {
-            label: "Engine",
-            key: "engine",
-            data: carDetails?.engine,
-          },
-          {
-            label: "Engine Power [HP]",
-            key: "enginePower",
-            data: carDetails?.enginePower,
-          },
-        ]}
-      />
-      <CarDetailBlock
-        image={"/wheel-icon.png"}
-        carId={carId}
-        details={[
-          {
-            label: "Powered Wheels",
-            key: "powerWheels",
-            data: carDetails?.powerWheels,
-            selection: [
-              { value: "Front", label: "Front" },
-              { value: "Rear", label: "Rear" },
-              { value: "All", label: "All" },
-            ],
-          },
-          {
-            label: "Drivetrain",
-            key: "drivetrain",
-            data: carDetails?.drivetrain,
-            selection: [
-              { value: "4WD", label: "4WD" },
-              { value: "2WD", label: "2WD" },
-            ],
-          },
-        ]}
-      />
+      <CarDocuments carId={carId} />
+      <CarDetails carId={carId} />
       <br />
-      <Title>Service History</Title>
-      <Services carId={carId} />
-      <Footer />
+      <Services carId={carId}>Service History</Services>
     </SecureContainer>
   );
 }
