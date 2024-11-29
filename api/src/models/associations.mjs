206c033a247138a3465fb8bd2faf643f7e3062a6
SamuelFoc <samo.sipikal@gmail.com>
2024-11-29 14:32:52 +0100

    Add car - car document association

--- api/src/models/associations.mjs
+++ api/src/models/associations.mjs
@@ -2,6 +2,7 @@ import User from "./User.mjs";
 import Car from "./Car.mjs";
 import ServicePart from "./ServicePart.mjs";
 import Service from "./Service.mjs";
+import CarDocument from "./CarDocument.mjs";
 
 // USER - CAR
 User.hasMany(Car, { foreignKey: "userId", onDelete: "CASCADE" });
@@ -13,14 +14,15 @@ ServicePart.belongsTo(Car, { foreignKey: "carId" });
 
 // CAR - SERVICE
 Car.hasMany(Service, { foreignKey: "carId", onDelete: "CASCADE" });
-Service.belongsTo(Car, { foreignKey: "carId", onDelete: "CASCADE" });
+Service.belongsTo(Car, { foreignKey: "carId" });
+
+// CAR - CAR DOCUMENT
+Car.hasMany(CarDocument, { foreignKey: "carId", onDelete: "CASCADE" });
+CarDocument.belongsTo(Car, { foreignKey: "carId" });
 
 // SERVICE - SERVICE PART
 Service.hasMany(ServicePart, { foreignKey: "serviceId", onDelete: "CASCADE" });
-ServicePart.belongsTo(Service, {
-  foreignKey: "serviceId",
-  onDelete: "CASCADE",
-});
+ServicePart.belongsTo(Service, { foreignKey: "serviceId" });
 
 export const setupAssociations = () => {
   // This function ensures associations are set up when needed
