aefe61469d5f01e45ecdfed9a7344436f8396f05
SamuelFoc <samo.sipikal@gmail.com>
2024-11-29 14:41:27 +0100

    Add wider attrubute to the RG C

--- ui/src/components/ui/ResponsiveGrid.jsx
+++ ui/src/components/ui/ResponsiveGrid.jsx
@@ -1,7 +1,13 @@
-export default function ResponsiveGrid({ children }) {
+export default function ResponsiveGrid({ children, wider }) {
+  const smCols = wider ? "sm:grid-cols-1" : "sm:grid-cols-2";
+  const mdCols = wider ? "md:grid-cols-2" : "md:grid-cols-3";
+  const lgCols = wider ? "lg:grid-cols-2" : "lg:grid-cols-3";
+
   return (
     <div className="w-full flex justify-evenly items-center">
-      <div className="w-full grid justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+      <div
+        className={`w-full grid justify-items-center grid-cols-1 ${smCols} ${mdCols} ${lgCols} gap-4`}
+      >
         {children}
       </div>
     </div>
